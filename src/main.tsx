e91ed6f440902392c046fe4d63b7f11e898ac807
glckfndr <obulan65@gmail.com>
2024-04-09 19:27:07 +0300

    feat: add QueryClient and QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
