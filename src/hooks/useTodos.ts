cff53e6b885b23314a64892ca5f009b54207348c
glckfndr <obulan65@gmail.com>
2024-04-26 21:49:31 +0300

    config: add ReactQueryDevtools and set staleTime

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -14,9 +14,11 @@ const useTodos = () => {
     axios
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
+
   return useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
+    staleTime: 10 * 1000,
   });
 };
 
